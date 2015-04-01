e90fa5541c23ea6200b145bccf191faf9a606401
SeanNguyen <firestormkjc.1993@gmail.com>
2015-04-01 14:57:29 +0800

    integrate PHP into project

---
+++ app/bidding_mgt.php
@@ -0,0 +1,163 @@
+<html lang="en"><script type="text/javascript">window["_gaUserPrefs"] = { ioo : function() { return true; } }</script><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Bidding Management</title>
+
+    <!-- build:css(.) styles/vendor.css -->
+    <!-- bower:css -->
+    <link rel="stylesheet" href="_/bower_components/bootstrap/dist/css/bootstrap.css" />
+    <!-- endbower -->
+    <!-- endbuild -->
+    <!-- build:css(.tmp) styles/bidding_mgt.css -->
+    <link rel="stylesheet" href="_/css/bidding_mgt.css">
+    <!-- endbuild -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <style type="text/css"></style></head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">NUS Centralised Online Registration System</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+        <!--
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+          -->
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Module Management</a></li>
+            <li class="active"><a href="#">Bidding Management <span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Bidding Management</h1>
+
+          <div class="table-responsive">
+            <table class="table">
+              <tr>
+                <td>
+                  <div id="cur_round"><b>Current Round: </b>1A </div>
+                  <div id="end_time"><b>End Time: </b>23 Jun 2015 17:52</div>
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <div id="p_acc"><b>Programme Account: </b>1850 </div>
+                  <div id="g_acc"><b>General Account: </b>443</div>
+                </td>
+              </tr>
+            </table>
+          </div>
+
+          <h2 class="sub-header">Modules Available in Current Round: </h2>
+          <h5>Note: Please enter 0 if you want to retract a bid. </h5>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>Module</th>
+                  <th>Vacancy</th>
+                  <th>Highest / Lowest Bid Point</th>
+                  <th>No. of Bidders</th>
+                  <th>Next Minimum Bid</th>
+                  <th>Your Bid</th>
+                  <th>Bid Status</th>
+                  <th>Account Type</th>
+                  <th>Place Bid</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>CS2102 DATABASE SYSTEMS</td>
+                  <td>46</td>
+                  <td>1000 / 3</td>
+                  <td>31</td>
+                  <td>1</td>
+                  <td>
+                  <form class="form-inline">
+                      <div class="form-group">
+                        <input type="text" class="form-control">
+                      </div>
+                    </form>
+                  </td>
+                  <td>Accepted</td>
+                  <td>Programme Account</td>
+                  <td><button type="submit" class="btn btn-default">Bid</button></td>
+                </tr>
+                <tr>
+                  <td>CS2103 SOFTWARE ENGINEERING</td>
+                  <td>46</td>
+                  <td>1000 / 3</td>
+                  <td>31</td>
+                  <td>1</td>
+                  <td>
+                      <div class="form-group">
+                        <input type="text" class="form-control">
+                      </div>
+                    </form>                    
+                  </td>
+                  <td>Accepted</td>
+                  <td>Programme Account</td>
+                  <td><button type="submit" class="btn btn-default">Bid</button></td>
+                </tr>
+                <tr>
+                  <td>GEK1002 INTRODUCTION TO JAPANESE STUDIES</td>
+                  <td>46</td>
+                  <td>1000 / 3</td>
+                  <td>31</td>
+                  <td>1</td>
+                  <td>
+                      <div class="form-group">
+                        <input type="text" class="form-control">
+                      </div>
+                    </form>                      
+                  </td>
+                  <td>Accepted</td>
+                  <td>General Account</td>
+                  <td><button type="submit" class="btn btn-default">Bid</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200" preserveAspectRatio="none" style="visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs></defs><text x="0" y="10" style="font-weight:bold;font-size:10pt;font-family:Arial, Helvetica, Open Sans, sans-serif;dominant-baseline:middle">200x200</text></svg></body></html>
